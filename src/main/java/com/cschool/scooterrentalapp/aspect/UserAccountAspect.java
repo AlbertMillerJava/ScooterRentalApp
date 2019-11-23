9e149a3c218996fd9b1bd345719fa03e7e95c75b
AlbertMillerJava <archalbertmiller@gmail.com>
2019-11-23 16:11:37 +0100

    add aspect around

--- src/main/java/com/cschool/scooterrentalapp/aspect/UserAccountAspect.java
+++ src/main/java/com/cschool/scooterrentalapp/aspect/UserAccountAspect.java
@@ -2,7 +2,9 @@ package com.cschool.scooterrentalapp.aspect;
 
 import com.cschool.scooterrentalapp.api.request.CreateUserAccountRequest;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,5 +20,22 @@ public class UserAccountAspect {
         CreateUserAccountRequest userAccountRequest = (CreateUserAccountRequest) returning.getArgs()[0];
 
         logger.info("user created with username = {}", userAccountRequest.getOwnerUserName());
+    }
+
+    @Around("execution(* com.cschool.scooterrentalapp.service.UserAccountServiceImpl.rechargeUserAccount(..))")
+    public void saveUserMethod(ProceedingJoinPoint proceedingJoinPoint){
+        try{
+            logger.info("start recharging user account balance");
+
+            Object[] args = proceedingJoinPoint.getArgs();
+            long userId = (long) args[0];
+
+            String amount = (String) proceedingJoinPoint.proceed(args);
+
+            logger.info("user with id ={} recharged his account with amount ={} ", userId, amount);
+        } catch (Throwable e){
+
+        }
+
     }
 }
